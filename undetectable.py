4ca8beac69579534500e95f81ab7761f045092ba
Michael Kitas <mixaliskitas@gmail.com>
2022-03-25 15:59:21 +0200

    Ep 6 - Bypass Detection using plugins, settings & proxies

---
+++ undetectable.py
@@ -0,0 +1,34 @@
+import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+if __name__ == '__main__':
+    email = "#"
+    password = "#"
+
+    options = webdriver.ChromeOptions()
+    #options.add_argument('proxy-server=106.122.8.54:3128')
+    options.add_argument('--user-data-dir=C:\Users\suppo\AppData\Local\Google\Chrome\User Data\Default')
+
+    browser = uc.Chrome(
+        options=options,
+    )
+    browser.get('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&hl=en&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+
+    browser.find_element(By.ID, 'identifierId').send_keys(email)
+
+    browser.find_element(
+        By.CSS_SELECTOR, '#identifierNext > div > button > span').click()
+
+    password_selector = "#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"
+
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, password_selector)))
+
+    browser.find_element(
+        By.CSS_SELECTOR, password_selector).send_keys(password)
+
+    browser.find_element(
+        By.CSS_SELECTOR, '#passwordNext > div > button > span').click()
